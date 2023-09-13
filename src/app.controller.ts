d8506023d70c66bd8134bea2f5760aa1e0f38054
twosdai <daniel.wasserlauf@paigo.tech>
2023-09-13 13:19:15 -0400

    kubernetes yml and health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller()
+@Controller('health')
+@ApiTags('HealthCheck')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @ApiOperation({ operationId: 'Health Check' })
+  @Get()
+  getHealthCheck() {
+    return this.appService.getHealthCheck();
+  }
 }
